d257a496e833f2f0b61f598a2d2885972a2c5546
umair-javeed <umairjaved790@gmail.com>
2025-11-03 15:07:36 -0500

    change authentication code

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,32 @@
 'use client';
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { Amplify } from 'aws-amplify';
 import { signIn, signOut, signUp, confirmSignUp, getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_tv8uaa8YJ',
+      userPoolClientId: '64b8d4lmcsicnadks6u9m8jke',
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: 'code' as const,
+      userAttributes: {
+        email: { required: true },
+        name: { required: true },
+      },
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    }
+  }
+}, { ssr: true });
 
 interface User {
   username: string;
